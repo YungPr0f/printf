cdc8c27f37217afcffc6a7e1b95ccce4ed43955b
Abigail <akinbamireabigail@gmail.com>
2022-10-18 04:54:53 -0700

    don't be afraid to show up

--- main.h
+++ main.h
@@ -38,5 +38,6 @@ int prinlunt(va_list arguments, char *buf, unsigned int ibuf);
 int prinloct(va_list arguments, char *buf, unsigned int ibuf);
 int prinlhex(va_list arguments, char *buf, unsigned int ibuf);
 int prinlupx(va_list arguments, char *buf, unsigned int ibuf);
+int prinhint(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
