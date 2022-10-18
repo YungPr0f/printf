5e5dc57c51f3a43f7e7c501408f97892b08528dc
Abigail <akinbamireabigail@gmail.com>
2022-10-18 03:51:49 -0700

    Consistency is an A game plan

--- main.h
+++ main.h
@@ -31,5 +31,6 @@ int print_hex(va_list arguments, char *buf, unsigned int ibuf);
 int print_upx(va_list arguments, char *buf, unsigned int ibuf);
 int print_usr(va_list arguments, char *buf, unsigned int ibuf);
 int print_add(va_list arguments, char *buf, unsigned int ibuf);
+int print_rev(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
