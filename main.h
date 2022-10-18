8e67f52b25fd5ee6c8e019f0ef1aa4a8de9f92b6
Abigail <akinbamireabigail@gmail.com>
2022-10-18 05:38:40 -0700

    you don't dwell on mistakes you learn from them

--- main.h
+++ main.h
@@ -46,5 +46,6 @@ int prinhupx(va_list arguments, char *buf, unsigned int ibuf);
 int prinpint(va_list arguments, char *buf, unsigned int ibuf);
 int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
 int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
+int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
