ad4ffe04ead0111d9f55ee307e107048fc53d3b2
Abigail <akinbamireabigail@gmail.com>
2022-10-18 05:14:10 -0700

    consistency remains the key to success

--- main.h
+++ main.h
@@ -42,5 +42,6 @@ int prinhint(va_list arguments, char *buf, unsigned int ibuf);
 int prinhunt(va_list arguments, char *buf, unsigned int ibuf);
 int prinhoct(va_list arguments, char *buf, unsigned int ibuf);
 int prinhhex(va_list arguments, char *buf, unsigned int ibuf);
+int prinhupx(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
