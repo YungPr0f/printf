e73f5c9c00240ba6ed9d61770f621ccfd74ac7ca
Abigail <akinbamireabigail@gmail.com>
2022-10-18 04:59:07 -0700

    Keep showing up

--- main.h
+++ main.h
@@ -39,5 +39,6 @@ int prinloct(va_list arguments, char *buf, unsigned int ibuf);
 int prinlhex(va_list arguments, char *buf, unsigned int ibuf);
 int prinlupx(va_list arguments, char *buf, unsigned int ibuf);
 int prinhint(va_list arguments, char *buf, unsigned int ibuf);
+int prinhunt(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
