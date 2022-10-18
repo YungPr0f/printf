962cec2d533ba8649fb84cff5e6edc3cb5a8ac89
Abigail <akinbamireabigail@gmail.com>
2022-10-18 04:18:24 -0700

    the blessings of teamwork cannot be unmatched

--- main.h
+++ main.h
@@ -35,5 +35,6 @@ int print_rev(va_list arguments, char *buf, unsigned int ibuf);
 int print_rot(va_list arguments, char *buf, unsigned int ibuf);
 int prinlint(va_list arguments, char *buf, unsigned int ibuf);
 int prinlunt(va_list arguments, char *buf, unsigned int ibuf);
+int prinloct(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
