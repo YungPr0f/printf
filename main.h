5c2f9378b16b62c4eb6f2e8cc919342f792d471a
Abigail <akinbamireabigail@gmail.com>
2022-10-18 05:27:09 -0700

    mistakes are often inevitable

--- main.h
+++ main.h
@@ -44,6 +44,6 @@ int prinhoct(va_list arguments, char *buf, unsigned int ibuf);
 int prinhhex(va_list arguments, char *buf, unsigned int ibuf);
 int prinhupx(va_list arguments, char *buf, unsigned int ibuf);
 int prinpint(va_list arguments, char *buf, unsigned int ibuf);
-int prinpint(va_list arguments, char *buf, unsigned int ibuf);
+int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
